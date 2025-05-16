5e3d1514d91aa3517d5322c19725186f83dfc47d
Minh Nguyen <vn.ndminh@gmail.com>
2025-05-16 14:15:01 -0500

    feat: upload hw-ica 14

---
+++ HW-ICA/HW14/src/mappings.py
@@ -0,0 +1,40 @@
+# mapping between the filenames of the images 
+# and the actual name of the person
+gallery_path = "gallery"
+gallery_mapping = {
+    gallery_path + "/Andrew_Garfield.jpg": "Andrew Garfield",
+    gallery_path + "/Robert_Downey_Jr.jpg": "Robert Downey Jr.",
+    gallery_path + "/Ryan_Reynolds.jpg": "Ryan Reynolds",
+    gallery_path + "/Lionel_Messi.jpg": "Lionel Messi",
+    gallery_path + "/Gordon_Ramsay.jpg": "Gordon Ramsay",
+}
+
+# mapping of probe images to the actual name of the person
+probe_path = "probe"
+probe_mapping = {
+    probe_path + "/Andrew_Garfield_1.jpg": "Andrew Garfield",
+    probe_path + "/Andrew_Garfield_2.jpg": "Andrew Garfield",
+    probe_path + "/Andrew_Garfield_3.jpg": "Andrew Garfield",
+    probe_path + "/Andrew_Garfield_4.jpg": "Andrew Garfield",
+    probe_path + "/Andrew_Garfield_5.jpg": "Andrew Garfield",
+    probe_path + "/Robert_Downey_Jr_1.jpg": "Robert Downey Jr.",
+    probe_path + "/Robert_Downey_Jr_2.jpg": "Robert Downey Jr.",
+    probe_path + "/Robert_Downey_Jr_3.jpg": "Robert Downey Jr.",
+    probe_path + "/Robert_Downey_Jr_4.jpg": "Robert Downey Jr.",
+    probe_path + "/Robert_Downey_Jr_5.jpg": "Robert Downey Jr.",
+    probe_path + "/Ryan_Reynolds_1.jpg": "Ryan Reynolds",
+    probe_path + "/Ryan_Reynolds_2.jpg": "Ryan Reynolds",
+    probe_path + "/Ryan_Reynolds_3.jpg": "Ryan Reynolds",
+    probe_path + "/Ryan_Reynolds_4.jpg": "Ryan Reynolds",
+    probe_path + "/Ryan_Reynolds_5.jpg": "Ryan Reynolds",
+    probe_path + "/Lionel_Messi_1.jpg": "Lionel Messi",
+    probe_path + "/Lionel_Messi_2.jpg": "Lionel Messi",
+    probe_path + "/Lionel_Messi_3.jpg": "Lionel Messi",
+    probe_path + "/Lionel_Messi_4.jpg": "Lionel Messi",
+    probe_path + "/Lionel_Messi_5.jpg": "Lionel Messi",
+    probe_path + "/Gordon_Ramsay_1.jpg": "Gordon Ramsay",
+    probe_path + "/Gordon_Ramsay_2.jpg": "Gordon Ramsay",
+    probe_path + "/Gordon_Ramsay_3.jpg": "Gordon Ramsay",
+    probe_path + "/Gordon_Ramsay_4.jpg": "Gordon Ramsay",
+    probe_path + "/Gordon_Ramsay_5.jpg": "Gordon Ramsay",
+}
